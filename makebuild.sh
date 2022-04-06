65a53cc21278975241501a8062d42cef471e9c18
vamman <vamman@gmail.com>
2022-04-06 13:19:28 -0300

    Update makebuild.sh

--- makebuild.sh
+++ makebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start build
-cd buildbot/runners/wine && ./build.sh --as $1 --version $3 --with $2 --branch $3 --useccache --staging --noupload --keep --dependencies
+cd buildbot/runners/wine && ./build.sh --as $1 --version $3 --with $2 --branch $3 --useccache --staging --noupload --keep --dependencies $4
