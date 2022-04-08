f3768b06ede59f16267f600b27e7166805dfd6fc
vamman <vamman@gmail.com>
2022-04-08 09:39:40 -0300

    Portable path for mingw

--- makebuild.sh
+++ makebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start build
-cd buildbot/runners/wine && ./build.sh --as $1 --version $3 --with $2 --branch $3 --dependencies $4 --useccache --64bit --noupload --keep
+cd buildbot/runners/wine && ./build.sh --as $1 --version $3 --with $2 --branch $3 --dependencies $4 --mingwportablepath $5 --useccache --usemingw  --64bit --noupload --keep
